cffa825249c675b31b891f43e598ac3c1e9e7a81
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-06-09 23:03:22 -0700

    Add nav links

--- flashcards/src/App.js
+++ flashcards/src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Card } from './features/card/Card';
-import { Category } from './features/category/Category.js';
-import { AddCategory } from './features/category/AddCategory.js';
-import { AddEditCard } from './features/card/AddEditCardForm';
-import { Recall } from './features/recall/Recall';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import React from "react";
+import { Card } from "./features/card/Card";
+import { Category } from "./features/category/Category.js";
+import { AddCategory } from "./features/category/AddCategory.js";
+import { AddEditCard } from "./features/card/AddEditCardForm";
+import { Recall } from "./features/recall/Recall";
+import { BrowserRouter, Switch, Route, NavLink, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <header>
+          <nav>
+            <NavLink to="/category">Category</NavLink>
+            <NavLink to="/card">Card</NavLink>
+          </nav>
+        </header>
         <Switch>
-          <Route exact path="/" component={Card}/>
-          <Route exact path="/category" component={Category}/>
-          <Route exact path="/category/add" component={AddCategory}/>
-          
-          <Route exact path="/card/add" component={AddEditCard}/>
-          <Route exact path="/card/edit/:id" component={AddEditCard}/>
-          <Route exact path="/recall/:id" component={Recall}/>
+          <Route exact path="/category" component={Category} />
+          <Route exact path="/card" component={Card} />
+          <Route exact path="/category" component={Category} />
+          <Route exact path="/category/add" component={AddCategory} />
+          <Route exact path="/card/add" component={AddEditCard} />
+          <Route exact path="/card/edit/:id" component={AddEditCard} />
+          <Route exact path="/recall/:id" component={Recall} />
+          <Route exact path="/">
+            <Redirect to="/category" />
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
