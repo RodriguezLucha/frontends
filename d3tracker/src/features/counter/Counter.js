dce75a6774ffd49ff1d1998182829043455a05e6
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-09-09 00:28:57 -0700

    Move bar with single button

--- d3tracker/src/features/counter/Counter.js
+++ d3tracker/src/features/counter/Counter.js
@@ -1,17 +1,16 @@
 import { useSelector, useDispatch } from "react-redux";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styles from "./Counter.module.css";
 import { select, scaleBand, scaleLinear, max } from "d3";
 import useResizeObserver from "../../useResizeObserver";
-
+const clone = require('rfdc')();
 
 export function Counter() {
-
   const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
 
-  let data = [
+  const [data, setData] = useState([
     {
       name: "alpha",
       value: 200,
@@ -24,46 +23,56 @@ export function Counter() {
       total: 100,
       color: "#cccccc"
     }
-  ];
+  ]);
 
-  useEffect(() => {
-    const svg = select(svgRef.current);
-    if (!dimensions) return;
+  useEffect(
+    () => {
+      const svg = select(svgRef.current);
+      if (!dimensions) return;
 
-    // sorting the data
-    data.sort((a, b) => b.value - a.value);
+      const yScale = scaleBand()
+        .paddingInner(0.1)
+        .domain(data.map((value, index) => index)) // [0,1,2,3,4,5]
+        .range([0, dimensions.height]); // [0, 200]
 
-    const yScale = scaleBand()
-      .paddingInner(0.1)
-      .domain(data.map((value, index) => index)) // [0,1,2,3,4,5]
-      .range([0, dimensions.height]); // [0, 200]
+      //CURRENT: This needs to be an array.
+      const xScales = data.map(item =>
+        scaleLinear().domain([0, item.total]).range([0, dimensions.width])
+      );
+      console.log(xScales);
 
-    //CURRENT: This needs to be an array.
-    const xScale = scaleLinear()
-      .domain([0, max(data, entry => entry.total)]) // [0, 65 (example)]
-      .range([0, dimensions.width]); // [0, 400 (example)]
-
-    // draw the bars
-    svg
-      .selectAll(".bar")
-      .data(data, (entry, index) => entry.name)
-      .join(enter =>
-        enter.append("rect").attr("y", (entry, index) => yScale(index))
-      )
-      .attr("fill", entry => entry.color)
-      .attr("class", "bar")
-      .attr("x", 0)
-      .attr("height", yScale.bandwidth())
-      .transition()
-      .attr("width", entry => xScale(entry.value))
-      .attr("y", (entry, index) => yScale(index));
-
-
-  }, [data, dimensions]);
+      // draw the bars
+      svg
+        .selectAll(".bar")
+        .data(data, (entry, index) => entry.name)
+        .join(enter =>
+          enter.append("rect").attr("y", (entry, index) => yScale(index))
+        )
+        .attr("fill", entry => entry.color)
+        .attr("class", "bar")
+        .attr("x", 0)
+        .attr("height", yScale.bandwidth())
+        .transition()
+        .attr("width", (entry, i) => xScales[i](entry.value))
+        .attr("y", (entry, index) => yScale(index));
+    },
+    [data, dimensions]
+  );
 
   return (
-    <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
-      <svg ref={svgRef}></svg>
+    <div>
+      <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+        <svg ref={svgRef} />
+      </div>
+      <button
+        onClick={() => {
+          let dataCopy = clone(data);
+          dataCopy[0].value+=1
+          setData(dataCopy);
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 }
